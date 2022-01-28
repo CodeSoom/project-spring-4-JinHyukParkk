c2eafc2d870a51e07ba752486c5694041aa5b53f
hyuk <pjh08190819@naver.com>
2022-01-28 23:25:36 +0900

    test : size 체크 테스트 로직 변경

--- cotobang/src/test/java/com/example/cotobang/controller/CoinControllerTest.java
+++ cotobang/src/test/java/com/example/cotobang/controller/CoinControllerTest.java
@@ -1,12 +1,14 @@
 package com.example.cotobang.controller;
 
+import com.example.cotobang.domain.Coin;
+import com.example.cotobang.respository.CoinRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -17,10 +19,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DisplayName("CoinController 클래스")
 class CoinControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    CoinRepository coinRepository;
+
+    @BeforeEach
+    void setUp() {
+        Coin coin = Coin.builder()
+                .koreanName("비트코인")
+                .build();
+
+        coinRepository.save(coin);
+    }
 
     @Nested
     @DisplayName("GET /coins 요청은")
@@ -29,9 +44,11 @@ class CoinControllerTest {
         @Test
         @DisplayName("200(Ok)와 Coin의 전체 리스트를 응답합니다.")
         void it_return_coins() throws Exception {
+            final int coinsSize = coinRepository.findAll().size();
+
             mockMvc.perform(get("/coins"))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$", hasSize(1)))
+                    .andExpect(jsonPath("$", hasSize(coinsSize)))
                     .andDo(print());
         }
     }
