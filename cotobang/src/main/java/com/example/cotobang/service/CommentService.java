e9630ff36127ff426cb34c3469445d40c5cc4d02
hyuk <pjh08190819@naver.com>
2022-02-17 02:45:59 +0900

    feat : CommentService 댓글 조회 기능 구현

--- cotobang/src/main/java/com/example/cotobang/service/CommentService.java
+++ cotobang/src/main/java/com/example/cotobang/service/CommentService.java
@@ -1,6 +1,8 @@
 package com.example.cotobang.service;
 
+import com.example.cotobang.domain.Coin;
 import com.example.cotobang.domain.Comment;
+import com.example.cotobang.respository.CommentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,7 +10,18 @@ import java.util.List;
 @Service
 public class CommentService {
 
+    private final CommentRepository commentRepository;
+
+    private final CoinService coinService;
+
+    public CommentService(CommentRepository commentRepository, CoinService coinService) {
+        this.commentRepository = commentRepository;
+        this.coinService = coinService;
+    }
+
     public List<Comment> getComments(Long coinId) {
-        return null;
+        Coin coin = coinService.getCoin(coinId);
+
+        return commentRepository.findByCoin(coin);
     }
 }
