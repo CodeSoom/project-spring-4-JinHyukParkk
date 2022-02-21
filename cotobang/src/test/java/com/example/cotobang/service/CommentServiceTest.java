14114521d9f62009caa1b9182e3fa42ce697570c
hyuk <pjh08190819@naver.com>
2022-02-22 03:10:35 +0900

    test : CommentService의 createComment 메소드 테스트 코드 작성

--- cotobang/src/test/java/com/example/cotobang/service/CommentServiceTest.java
+++ cotobang/src/test/java/com/example/cotobang/service/CommentServiceTest.java
@@ -3,12 +3,14 @@ package com.example.cotobang.service;
 import com.example.cotobang.domain.Coin;
 import com.example.cotobang.domain.Comment;
 import com.example.cotobang.domain.User;
+import com.example.cotobang.dto.CommentDto;
 import com.example.cotobang.fixture.CoinFixtureFactory;
 import com.example.cotobang.fixture.CommentFixtureFactory;
 import com.example.cotobang.fixture.UserFixtureFactory;
 import com.example.cotobang.respository.CoinRepository;
 import com.example.cotobang.respository.CommentRepository;
 import com.example.cotobang.respository.UserRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -40,11 +42,18 @@ class CommentServiceTest {
 
     CommentFixtureFactory commentFixtureFactory;
 
+    Coin coin;
+
+    User user;
+
     @BeforeEach
     void setUp() {
         coinFixtureFactory = new CoinFixtureFactory();
         userFixtureFactory = new UserFixtureFactory();
         commentFixtureFactory = new CommentFixtureFactory();
+
+        coin = coinRepository.save(coinFixtureFactory.create_코인());
+        user = userRepository.save(userFixtureFactory.create_사용자());
     }
 
     @Nested
@@ -59,8 +68,7 @@ class CommentServiceTest {
 
             @BeforeEach
             void prepare() {
-                givenCoin = coinRepository.save(coinFixtureFactory.create_코인());
-                User user = userRepository.save(userFixtureFactory.create_사용자());
+                givenCoin = coin;
 
                 Comment comment = commentFixtureFactory.create_댓글(givenCoin, user);
 
@@ -72,6 +80,36 @@ class CommentServiceTest {
             void it_return_comments() throws Exception {
                 assertThat(commentService.getComments(givenCoin))
                         .hasSize(1);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("createComment() 메소드는")
+    class Describe_createComment {
+
+        @Nested
+        @DisplayName("Comment 정보와 coin id와 user id가 주어진다면")
+        class Context_with_comment_and_coin_id_and_user_id {
+
+            CommentDto givenCommentDto;
+
+            @BeforeEach
+            void prepare() {
+                givenCommentDto = commentFixtureFactory.create_댓글_요청_DTO(
+                        coin.getId(),
+                        user.getId()
+                );
+            }
+
+            @Test
+            @DisplayName("Comment를 생성하고 리턴합니다.")
+            void it_create_comment_reture_comment() {
+                Comment comment = commentService.createComment(givenCommentDto);
+
+                assertThat(comment).isNotNull();
+                assertThat(comment.getComment())
+                        .isEqualTo(givenCommentDto.getComment());
             }
         }
     }
