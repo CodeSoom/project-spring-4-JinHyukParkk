f1f14ad9b3728ef357ff53a557eda0f137588705
hyuk <pjh08190819@naver.com>
2022-02-22 23:07:48 +0900

    feat : CommentService 댓글 생성 구현

--- cotobang/src/main/java/com/example/cotobang/service/CommentService.java
+++ cotobang/src/main/java/com/example/cotobang/service/CommentService.java
@@ -4,11 +4,7 @@ import com.example.cotobang.domain.Coin;
 import com.example.cotobang.domain.Comment;
 import com.example.cotobang.domain.User;
 import com.example.cotobang.dto.CommentDto;
-import com.example.cotobang.errors.CoinNotFoundException;
-import com.example.cotobang.errors.UserNotFoundException;
-import com.example.cotobang.respository.CoinRepository;
 import com.example.cotobang.respository.CommentRepository;
-import com.example.cotobang.respository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,32 +16,15 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    private final CoinRepository coinRepository;
-
-    private final UserRepository userRepository;
-
-    public CommentService(CommentRepository commentRepository,
-                          CoinRepository coinRepository,
-                          UserRepository userRepository) {
+    public CommentService(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
-        this.coinRepository = coinRepository;
-        this.userRepository = userRepository;
     }
 
     public List<Comment> getComments(Coin coin) {
         return commentRepository.findByCoin(coin);
     }
 
-    public Comment createComment(CommentDto commentDto) {
-        Long coinId = commentDto.getCoinId();
-        Long userId = commentDto.getUserId();
-
-        Coin coin = coinRepository.findById(coinId)
-                .orElseThrow(() -> new CoinNotFoundException(coinId));
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException(userId));
-
+    public Comment createComment(CommentDto commentDto, Coin coin, User user) {
         Comment comment = Comment.builder()
                 .comment(commentDto.getComment())
                 .coin(coin)
