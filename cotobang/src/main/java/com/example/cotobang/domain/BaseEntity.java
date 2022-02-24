7bd915bab9005f1c214ec043edde6d59aa9d2a2d
hyuk <pjh08190819@naver.com>
2022-02-24 22:52:35 +0900

    feat: 생성 / 수정 시간 자동 추가 기능 구현

---
+++ cotobang/src/main/java/com/example/cotobang/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.cotobang.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(columnDefinition = "datetime(6) default now(6) comment '생성시간'", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(columnDefinition = "datetime(6) default now(6) comment '수정시간'", nullable = false)
+    private LocalDateTime updatedAt;
+}
+
