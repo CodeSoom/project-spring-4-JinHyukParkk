98f2946e08f90a3a7a28cdceb7bc235d149b995e
hyuk <pjh08190819@naver.com>
2022-03-03 22:45:49 +0900

    test : 유효하지 않는 Token 테스트 코드 작성

--- cotobang/src/test/java/com/example/cotobang/utils/JwtUtilTest.java
+++ cotobang/src/test/java/com/example/cotobang/utils/JwtUtilTest.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.security.SignatureException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @DisplayName("JwtUtil 클래스")
@@ -16,10 +19,13 @@ class JwtUtilTest {
     @Autowired
     private JwtUtil jwtUtil;
 
-    Long givenUserId = 1L;
-    String givenValidToken = "eyJhbGciOiJIUzI1NiJ9." +
+    private static final Long USER_ID = 1L;
+    private static final String VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
             "eyJ1c2VySWQiOjF9.PdEMJWhmPP4redDYU1ovusV_" +
             "5el6JSQW5D2CGiWqUOE";
+    private static final String INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJ1c2VySWQiOjF9.PdEMJWhmPP4redDYU1ovusV_" +
+            "5el6JSQW5D2CGiWqUOE123asd";
 
     @Nested
     @DisplayName("encode() 메소드는")
@@ -28,22 +34,39 @@ class JwtUtilTest {
         @Test
         @DisplayName("token을 리턴합니다.")
         void it_return_token() {
-            String token = jwtUtil.encode(givenUserId);
+            String token = jwtUtil.encode(USER_ID);
 
-            assertThat(token).isEqualTo(givenValidToken);
+            assertThat(token).isEqualTo(VALID_TOKEN);
         }
     }
 
     @Nested
     @DisplayName("decode() 메소드는")
     class Describe_decode {
-        
-        @Test
-        @DisplayName("userId 정보를 리턴합니다.")
-        void it_return_userId() {
-            Claims claims = jwtUtil.decode(givenValidToken);
 
-            assertThat(claims.get("userId")).isEqualTo(givenUserId);
+        @Nested
+        @DisplayName("유효한 Token이 주어진다면")
+        class Context_with_token {
+
+            @Test
+            @DisplayName("userId 정보를 리턴합니다.")
+            void it_return_userId() {
+                Claims claims = jwtUtil.decode(VALID_TOKEN);
+
+                assertThat(claims.get("userId")).isEqualTo(USER_ID);
+            }
+        }
+
+        @Nested
+        @DisplayName("유효하지 않는 Token이 주어진다면")
+        class Context_with_invalid_token {
+
+            @Test
+            @DisplayName("SignatureException 던집니다.")
+            void it_return_signatureException() {
+                assertThatThrownBy(() -> jwtUtil.decode(INVALID_TOKEN))
+                        .isInstanceOf(SignatureException.class);
+            }
         }
     }
 }
