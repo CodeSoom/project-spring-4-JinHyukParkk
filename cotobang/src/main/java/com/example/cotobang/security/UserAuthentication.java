c242e806b399cd8831bb0d6a7f202f7698eddeae
hyuk <pjh08190819@naver.com>
2022-04-04 23:59:24 +0900

    chore : 안쓰는 import 제거

--- cotobang/src/main/java/com/example/cotobang/security/UserAuthentication.java
+++ cotobang/src/main/java/com/example/cotobang/security/UserAuthentication.java
@@ -5,7 +5,6 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
